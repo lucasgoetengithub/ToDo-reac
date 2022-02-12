0c3090e7a90fd01ce879ca4f7d428a5ca28e797d
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-02-12 11:21:10 -0300

    primeiras configurações styled

---
+++ src/views/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as S from './styles'
+
+//Componentes
+import Header from '../../components/Header';
+
+function Home() {
+    return (
+        <S.Container>
+            <Header/>
+        </S.Container>
+    )
+}
+
+export default Home;
