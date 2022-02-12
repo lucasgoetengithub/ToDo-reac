52f55760f19a3aa4686043d9e9175924903863fd
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-02-12 11:29:08 -0300

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import * as S from './styles'
+
+import logo from '../../assets/logo.png';
 
 function Header() {
-    return <h1>HEADER AQUI</h1>
+    return (
+        <S.Container>
+            <S.LeftSide>
+                <image src={logo} alt="Logo"/>
+            </S.LeftSide>
+            <S.RigthSide>
+                
+            </S.RigthSide>
+        </S.Container>
+    )
 }
 
 export default Header;
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,1 +1,21 @@
 import styled from 'styled-components';
+
+
+export const Container = styled.div`
+    width:100%;
+    height: 70px;
+    background: #20295F;
+    border-bottom:3px solid #EE6B26;
+    display: flex;
+`
+
+
+export const LeftSide = styled.div`
+    width: 50%;
+    height: 70px;
+`
+
+export const RigthSide = styled.div`
+    width: 50%;
+    height: 70px;
+`
