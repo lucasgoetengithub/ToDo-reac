aa9c31450ea3b93ecf09f5abdbfe157ba65caf30
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-02-14 19:43:42 -0300

    Footer

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -3,11 +3,13 @@ import * as S from './styles'
 
 //Componentes
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 function Home() {
     return (
         <S.Container>
             <Header/>
+            <Footer/>
         </S.Container>
     )
 }
